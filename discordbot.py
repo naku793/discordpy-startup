565e953cd834c2183844f81bc43c4cba4bc79560
naku793 <57907039+naku793@users.noreply.github.com>
2021-05-28 01:21:15 +0900

    test01

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,8 @@
 from discord.ext import commands
 import os
 import traceback
+import random
+import asyncio #sleepを使うのに必要
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -16,6 +18,19 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    
+if message.content == "スロット":
+kakuritu = random.randint(1, 399)
+slot_list = ['\U00002660', '\U00002663', '\U00002665', '\U00002666', ':seven:']
+A = random.choice(slot_list)
+B = random.choice(slot_list)
+C = random.choice(slot_list)
+if int(kakuritu) == int(1): #確率は1 /399
+await client.send_message(message.channel, "ボーナス確定！！！")
+await asyncio.sleep(3) #3秒間待ってやる
+await client.send_message(message.channel, ':seven:', ':seven:', ':seven:') #7だけ出るように指定
+else:
+await client.send_message(message.channel, "%s%s%s" % (A, B, C))
 
 
 bot.run(token)
